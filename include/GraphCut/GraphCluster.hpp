b7843dcd66e1656988f7e480ef9c12e42ce145ed
AIBluefisher <1701213988@pku.edu.cn>
2018-06-23 23:42:52 +0800

    complete comments of GraphCluster

--- include/GraphCut/GraphCluster.hpp
+++ include/GraphCut/GraphCluster.hpp
@@ -4,100 +4,96 @@
 #include <string>
 #include <fstream>
 #include <cstring>
-// #include <vector>
 #include <memory>
 #include <queue>
 #include <utility>
 
 #include "ImageGraph.hpp"
 
-
-
 using namespace std;
 using namespace bluefish;
-
-
 
 
 namespace bluefish {
 class GraphCluster 
 {
 public:
-  size_t graphUpper;
-  float completeRatio;
+  size_t graphUpper;    // upper bound of cluster size
+  float completeRatio;  // completeness ratio
 
 public:
 
 GraphCluster(size_t upper = 100, float cr = 0.7);
 /**
- * @brief
- * @param imageList:
- * @param vocFile:
- * @return
+ * @brief Build a graph according to image list and vocabulary file(*.out)
+ * @param imageList: a file that stores the paths of images
+ * @param vocFile: vocabulary tree search file that store the similarity scores
+ * @return ImageGraph
  */
 ImageGraph BuildGraph(string imageList, string vocFile);
 
 /**
- * @brief
- * @param imageGraph:
+ * @brief Generate a file that stores the result of Normalized-Cut
+ * @param imageGraph: ImageGraph that represents the similarity information of images
  * @param dir: directory that store the normalized-cut files
- * @return
+ * @return The path of the file that stores the result of Normalized-Cut
  */
 string GenerateNCGraph(ImageGraph imageGraph, string dir);
 
 /**
- * @brief
- * @param filename:
- * @param clusterNum:
- * @return
+ * @brief Normalized-Cut interface that encapusulates the original algorithm of 
+ * Graclus library
+ * @param filename: normalized-cut file (produced by GenerateNCGraph function)
+ * @param clusterNum: the number of clusters that we want to divide into.
+ * @return Cluster results that represents the cluster ID (For example, return[0] = 1 
+ * suggests that image 0 belongs to 1-st cluster)
  */
 vector<size_t> NormalizedCut(string filename, size_t clusterNum);  
 
 /**
- * @brief Naive graph cluster
- * @param imageGraph:
- * @param clusters:
- * @param clusterNum:
+ * @brief Move images into different clusters
+ * @param imageGraphs: a queue that stores all the ImageGraph after GraphCluster algorithm 
+ * @param dir: root directory that you want to store the cluster result
  */
 void MoveImages(queue<shared_ptr<ImageGraph>> imageGraphs, string dir);  
 
 /**
- * @brief Naive graph cluster
- * @param imageGraph:
- * @param clusters:
- * @param clusterNum:
+ * @brief Move images into different clusters
+ * @param imageGraphs: a queue that stores all the ImageGraph after GraphCluster algorithm 
+ * @param dir: root directory that stores the cluster result
  */
 void MoveImages(vector<shared_ptr<ImageGraph>> imageGraphs, string dir);  
 
 /**
- * @brief
- * @param imageGraph
+ * @brief Bi-Partition the original image graph
+ * @param imageGraph: image graph that be partioned
+ * @param dir: directory that stores the normalized-cut file
  * @return
  */
 pair<shared_ptr<ImageGraph>, shared_ptr<ImageGraph>> BiPartition(ImageGraph imageGraph, string dir); 
 
 /**
- * @brief
- * @param igraphs
- * @param edge:
- * @return 
+ * @brief Judge if graphs has "edge"
+ * @param graphs: a list of image graphs
+ * @param edge: edge between nodes
+ * @return True if graphs has "edge"
  */
 bool HasEdge(vector<shared_ptr<ImageGraph>> graphs, LinkEdge edge);  
 
 /**
- * @brief
- * @param igraphs
- * @param edge:
- * @return 
+ * @brief Judge if graphs has "edge"
+ * @param graphs: a list of image graphs
+ * @param edge: edge between nodes
+ * @return True if graphs has "edge"
  */
 bool HasEdge(queue<shared_ptr<ImageGraph>> graphs, LinkEdge edge);
 
 /**
  * @brief Collect discarded edges in graph division
- * @param imageGraph:
- * @param insizeGraphs:
- * @param candidateGraphs:
- * @return
+ * @param imageGraph: the complete image graph before graph cluster
+ * @param insizeGraphs: graphs that node size less than graphUpper
+ * @param candidateGraphs: candidate graphs that node size may less than graphUpper
+ * @return An priority_queue that store all the discarded edges in graph division
  */
 priority_queue<LinkEdge> DiscardedEdges(ImageGraph imageGraph, 
                                         vector<shared_ptr<ImageGraph>> insizeGraphs, 
@@ -105,47 +101,55 @@ priority_queue<LinkEdge> DiscardedEdges(ImageGraph imageGraph,
 
 /**
  * @brief Select a image graph randomly which satisfy the completeness ratio
- * @param graphs:
- * @param edge:
- * @return
+ * param imageGraph: image graph
+ * @param graphs: a list of image graphs
+ * @param edge: edge between nodes
+ * @return Unselected node and the image graph that the selected node belongs to
  */
-pair<ImageNode, shared_ptr<ImageGraph>> SelectCRGraph(ImageGraph imageGraph, vector<shared_ptr<ImageGraph>> graphs, LinkEdge edge); 
+pair<ImageNode, shared_ptr<ImageGraph>> SelectCRGraph(ImageGraph imageGraph, 
+                                                      vector<shared_ptr<ImageGraph>> graphs, 
+                                                      LinkEdge edge); 
 
 /**
- * 
+ * @brief Count the number of common images between two clusters
+ * @param imageGraphL: left image graph
+ * @param imageGraphR: right image graph
+ * @return The number of common images between two clusters
  */
 int RepeatedNodeNum(const ImageGraph imageGraphL, const ImageGraph imageGraphR); 
 
 /**
- * @brief 
- * @param imageGraph:
- * @param ncFile:
- * @param clusterNum:
+ * @brief Graph Cluster that uses the graph expansion algorithm
+ * @param imageGraph: original image graph
+ * @param imageGraphs: initial image graphs that divided with no common images
+ * @param clusterNum: the number of clusters that divided into
+ * @return A list of image graphs after expansion-graph-cluster algorithm
  */
-vector<shared_ptr<ImageGraph>> ExpanGraphCluster(ImageGraph imageGraph, queue<shared_ptr<ImageGraph>> imageGraphs, string dir, size_t clusterNum); 
+vector<shared_ptr<ImageGraph>> ExpanGraphCluster(ImageGraph imageGraph, 
+                                                queue<shared_ptr<ImageGraph>> imageGraphs, 
+                                                string dir, size_t clusterNum); 
 
 /**
- * 
- * 
- * 
+ * @brief Naive graph cluster algorithm (all the clusters have no common images)
+ * @param imageGraphs: initial image graphs that divided with no common images
+ * @param dir: directory that store the images of clusters
+ * @param clusterNum: the number of clusters that divided into
+ * @return A list of image graphs after expansion-graph-cluster algorithm
  */
-vector<shared_ptr<ImageGraph>> NaiveGraphCluster(queue<shared_ptr<ImageGraph>> imageGraphs, string dir, size_t clusterNum); 
+vector<shared_ptr<ImageGraph>> NaiveGraphCluster(queue<shared_ptr<ImageGraph>> imageGraphs, 
+                                                string dir, size_t clusterNum);
 
 /**
- * 
- * 
- * 
+ * @brief Construct sub-imageGraphs according to the normalized-cut results
+ * @param imageGraph: image graph
+ * @param clusters: normalized-cut results that represents the cluster ID 
+ * (For example, return[0] = 1 suggests that image 0 belongs to 1-st cluster)
+ * @param clusterNum: the number of clusters that divided into
+ * @return A list of sub-imageGraphs
  */
-// void GraphCluster(const ImageGraph imageGraph, string dir, size_t clusterNum); 
-
-/**
- * @brief
- * @param imageGraph
- * @param clusters
- * @param clusterNum
- * @return
- */
-queue<shared_ptr<ImageGraph>> ConstructSubGraphs(ImageGraph imageGraph, vector<size_t> clusters, size_t clusterNum); 
+queue<shared_ptr<ImageGraph>> ConstructSubGraphs(ImageGraph imageGraph, 
+                                                vector<size_t> clusters, 
+                                                size_t clusterNum); 
 };
 
 }   // namespace bluefish
